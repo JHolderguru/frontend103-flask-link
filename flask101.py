5039b14b470ecb76a8a493dd8dc3764d4c4f8e1b
JHolderguru <JHolder@spartaglobal.com>
2020-06-18 21:00:40 +0100

    flask and bootstrap web complete

--- flask101.py
+++ flask101.py
@@ -1,10 +1,39 @@
-from flask import Flask
-
+from flask import Flask,render_template
 app = Flask(__name__)
+
+posts = [
+{
+	'author' :'Jonathan Holder',
+	'title':'Day in the life...',
+	'content':'Day in the life of a DevOps Engineer',
+	'date_posted':'June 2020'
+
+},
+
+]
+
+
+
 
 
 @app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
+@app.route("/home")
+
+def home():
+    return render_template('home.html',posts=posts)
+
+
+@app.route("/About")
+def about():
+    return  render_template('about.html', title='About')
+
+if __name__=='__main__':
+	app.run(debug=True)
+
+
+
+
+
+
 
 
